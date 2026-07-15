67e01ff94d8ed76c419cb5427862aa0ffb747dbb
kngght <absslknght@gmail.com>
2026-07-15 16:49:03 +0300

    Add: merge-strings-alternately - submission-8

---
+++ Data Structures & Algorithms/merge-strings-alternately/submission-8.go
@@ -0,0 +1,21 @@
+func mergeAlternately(word1 string, word2 string) string {
+	res := make([]byte, 0)
+	idx := 0
+
+	for idx < max(len(word1), len(word2)) {
+		if idx >= len(word1) {
+			res = append(res, word2[idx])
+			idx++
+			continue
+		}
+		if idx >= len(word2) {
+			res = append(res, word1[idx])
+			idx++
+			continue
+		}
+		res = append(res, word1[idx])
+		res = append(res, word2[idx])
+		idx++
+	}
+	return string(res)
+}
