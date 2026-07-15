1bef6582010acaf68d83ad8bfe8cea9c72237b6d
kngght <absslknght@gmail.com>
2026-07-15 18:12:11 +0300

    Add: remove-duplicates-from-sorted-array - submission-5

---
+++ Data Structures & Algorithms/remove-duplicates-from-sorted-array/submission-5.go
@@ -0,0 +1,10 @@
+func removeDuplicates(nums []int) int {
+	count := 1
+	for i := 1; i < len(nums); i++ {
+		if nums[i] != nums[i-1] {
+			nums[count] = nums[i]
+			count++
+		}
+	}
+	return count
+}
