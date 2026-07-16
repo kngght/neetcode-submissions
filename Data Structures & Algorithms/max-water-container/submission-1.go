af8ea31a39c0fb3de16e99ab8d2aad5cdff6cbaf
kngght <absslknght@gmail.com>
2026-07-17 01:17:50 +0300

    Add: max-water-container - submission-1

---
+++ Data Structures & Algorithms/max-water-container/submission-1.go
@@ -0,0 +1,13 @@
+func maxArea(heights []int) int {
+	left, right := 0, len(heights)-1
+	count := min(heights[left], heights[right])*(right-left)
+	for left < right {
+		count = max(count, min(heights[left], heights[right])*(right-left))
+		if heights[left] < heights[right] {
+			left++
+		} else {
+			right--
+		}
+	}
+	return count
+}
