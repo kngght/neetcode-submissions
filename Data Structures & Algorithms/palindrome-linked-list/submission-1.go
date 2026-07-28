d915d08e23504f38c92fb311a37d2e15ad98fdca
kngght <absslknght@gmail.com>
2026-07-29 02:11:52 +0300

    Add: palindrome-linked-list - submission-1

---
+++ Data Structures & Algorithms/palindrome-linked-list/submission-1.go
@@ -0,0 +1,51 @@
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+
+func isPalindrome(head *ListNode) bool {
+	fast, slow := head, head
+	for fast != nil && fast.Next != nil {
+		fast = fast.Next.Next
+		slow = slow.Next
+	}
+	var prev *ListNode
+	curr := slow
+	for curr != nil {
+		next := curr.Next
+		curr.Next = prev 
+		prev = curr
+		curr = next	
+	}
+
+	left := head
+	right := prev
+	for right != nil {
+		if left.Val != right.Val {
+			return false
+		}	
+		left = left.Next	
+		right = right.Next
+	} 
+	return true
+}
+
+// func isPalindrome(head *ListNode) bool {
+// 	result := true
+// 	var recurse func(l *ListNode)
+// 	recurse = func(l *ListNode) {
+// 		if l == nil {
+// 			return
+// 		}
+// 		recurse(l.Next)
+// 		if head.Val != l.Val {
+// 			result = false
+// 		}
+// 		head = head.Next
+// 	}
+// 	recurse(head)
+// 	return result
+// }
