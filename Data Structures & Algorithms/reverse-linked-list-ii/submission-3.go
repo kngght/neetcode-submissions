d3d7e4013ef0a970b9e1f28427f38716a3558751
kngght <absslknght@gmail.com>
2026-07-31 15:08:50 +0300

    Add: reverse-linked-list-ii - submission-3

---
+++ Data Structures & Algorithms/reverse-linked-list-ii/submission-3.go
@@ -0,0 +1,47 @@
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+ 
+// func reverseBetween(head *ListNode, left int, right int) *ListNode {
+// 	var rightHead *ListNode
+// 	var reverse func(head *ListNode, n int) *ListNode
+// 	reverse = func(head *ListNode, n int) *ListNode {
+// 		if n == 1 {
+// 			rightHead = head.Next
+// 			return head
+// 		}
+// 		newHead := reverse(head.Next, n-1)	
+// 		head.Next.Next = head
+// 		head.Next = rightHead
+// 		return newHead
+// 	}
+// 	if left == 1 {
+// 		return reverse(head, right)
+// 	}
+// 	head.Next = reverseBetween(head.Next, left-1, right-1)
+// 	return head
+// }
+
+
+func reverseBetween(head *ListNode, left int, right int) *ListNode {
+	dummy := &ListNode{Next: head}
+	prevSublist := dummy
+	for i := 1; i < left; i++ {
+		prevSublist = prevSublist.Next
+	}
+	var prev *ListNode
+	curr := prevSublist.Next
+	for i := 0; i < right - left + 1; i++ {
+		next := curr.Next
+		curr.Next = prev
+		prev = curr
+		curr = next
+	}
+	prevSublist.Next.Next = curr
+	prevSublist.Next = prev
+	return dummy.Next
+}
