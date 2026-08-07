f48a769ea3bc7ae340c83ccffb68e6f0fdcce601
kngght <absslknght@gmail.com>
2026-08-07 16:22:15 +0300

    Add: reverse-nodes-in-k-group - submission-7

---
+++ Data Structures & Algorithms/reverse-nodes-in-k-group/submission-7.go
@@ -0,0 +1,41 @@
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+
+func reverseKGroup(head *ListNode, k int) *ListNode {
+	dummy := &ListNode{Next: head}
+	groupPrev := dummy
+	for {
+		kthNode := groupPrev.getKth(k)
+		if kthNode == nil {
+			break
+		}
+		groupHead := kthNode.Next
+		oldHead := groupPrev.Next 
+		prev := kthNode.Next
+		curr := groupPrev.Next
+		for curr != groupHead {
+			next := curr.Next
+			curr.Next = prev
+			prev = curr
+			curr = next
+		} 
+		groupPrev.Next = prev
+		groupPrev = oldHead
+	}
+	return dummy.Next
+}
+
+func(l *ListNode) getKth(k int) *ListNode {
+	for i := 0; i < k; i++ {
+		if l == nil {
+			return l
+		}
+		l = l.Next
+	}
+	return l 
+}
