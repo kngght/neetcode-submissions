4ccb7382de8e2de09ed858f9f23fec9713a2e551
kngght <absslknght@gmail.com>
2026-08-11 11:02:00 +0300

    Add: insertion-sort-list - submission-5

---
+++ Data Structures & Algorithms/insertion-sort-list/submission-5.go
@@ -0,0 +1,62 @@
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+ 
+// func insertionSortList(head *ListNode) *ListNode {
+//     dummy := &ListNode{}
+//     newList := dummy
+//     for head != nil {
+//         prevMin, minNode := (*ListNode)(nil), head
+//         prev, curr := head, head.Next
+//         for curr != nil {
+//             if curr.Val < minNode.Val {
+//                 prevMin = prev
+//                 minNode = curr
+//             }
+//             prev = curr
+//             curr = curr.Next
+//         }
+//         if prevMin == nil {
+//             head = head.Next 
+//         } else {
+//             prevMin.Next = minNode.Next
+//         }
+//         newList.Next = minNode
+//         newList = newList.Next
+//     }
+//     newList.Next = nil
+//     return dummy.Next
+// }
+
+// func insertionSortList(head *ListNode) *ListNode {
+//     dummy := &ListNode{}
+//     for head != nil {
+//         curr := head
+//         head = head.Next
+//         curr.Next = nil
+//         prev := dummy
+//         for prev.Next != nil && prev.Next.Val < curr.Val {
+//             prev = prev.Next
+//         }
+//         curr.Next = prev.Next
+//         prev.Next = curr
+//     }
+//     return dummy.Next
+// }
+
+func insertionSortList(head *ListNode) *ListNode {
+    for curr := head; curr != nil; curr = curr.Next {
+        smallest := curr 
+        for tmp := curr; tmp != nil; tmp = tmp.Next {
+            if tmp.Val < smallest.Val {
+                smallest = tmp
+            }
+        }
+        curr.Val, smallest.Val = smallest.Val, curr.Val
+    } 
+    return head
+}
