94080941a231d576024f0bd13893589ac86e481b
kngght <absslknght@gmail.com>
2026-08-14 11:55:55 +0300

    Add: remove-linked-list-elements - submission-1

---
+++ Data Structures & Algorithms/remove-linked-list-elements/submission-1.go
@@ -0,0 +1,20 @@
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+
+func removeElements(head *ListNode, val int) *ListNode {
+	dummy := &ListNode{Next: head}
+	prev := dummy
+	for prev.Next != nil {
+		if prev.Next.Val == val {
+			prev.Next = prev.Next.Next
+		} else {
+			prev = prev.Next
+		}
+	}
+	return dummy.Next
+}
